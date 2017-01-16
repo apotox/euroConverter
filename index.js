290b0ca43f6ec2994e5eb9a9eb888b13ab4f6c50
kaka_root <saphidev@gmail.com>
2017-01-16 11:08:16 +0100

    slackeuro query console

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
 var express=require("express")
 var app=new express();
 
-app.all(/\/js\/.*js$/, function (req,res, next) {
 
-   //ngsafi.herokuapp.com
-   if(!req.header('Referer') || req.header('Referer').indexOf("ngsafi.herokuapp.com")<0){
-     res.status(403).send({
-        message: 'ngSafi Access Forbidden'
-     });
-   }else next();
-
-
-});
 
 
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
 
-app.get("/euro",function(req,res){
+app.get("/slack/",function(req,res){
   
+	console.log(req.query);
 
-
-
-  res.status(200).send({message:"euro"});
+//{message:req.query["r"]}
+  res.status(200).send("thank you");
 })
 
+
+
+
 app.get("/",function(req,res){
-	
-	res.render("test");
+
+	res.render("welcome");
 
 })
 
 var server=app.listen(process.env.PORT || 8080,function(){
-  console.log("start waiting x")
+  console.log("start waiting x slackeuro")
 });
