791d7bf02e84001397c07c851781defcc19a88f8
kaka_root <saphidev@gmail.com>
2017-01-16 11:50:58 +0100

    mataf

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express=require("express")
 var app=new express();
-
+var request = require('request');
 
 
 
@@ -10,9 +10,40 @@ app.use(express.static(__dirname+"/public"));
 app.get("/slack/",function(req,res){
   
 	console.log(req.query);
+	var result =null;
+	var reg=/^(\d+)\s([A-Z]+)\sto\s([A-Z]+)$/;
 
-//{message:req.query["r"]}
-  res.status(200).send("thank you");
+
+	if(req.query && req.query["text"])
+	{
+		var text=req.query["text"];
+		result = text.match(reg);
+		if(result){
+			request('https://www.mataf.net/fr/conversion/monnaie-EUR-DZD?m1='+result[1], function (error, response, body) {
+			  if (!error && response.statusCode == 200) {
+			    //console.log(body)
+			    //<title>100 Euro = 11764.7 Dinar Algérien (EUR/DZD = 117.59) - Mataf</title>
+
+			    var arr = /<title>(.*)<\/title>/gm.exec(body);
+			    if(arr){
+			    	res.status(200).send("well "+req.query["user_name"]+", "+arr[1]);
+			    }else{
+			    	res.status(200).send("sorry "+req.query["user_name"]+" nothing to show :(");
+			    }
+			    
+			  }
+			})
+		}else{
+		res.status(200).send("bad text");
+		}
+		
+	}else{
+		res.status(200).send("bad text");
+	}
+	
+	
+
+   
 })
 
 
